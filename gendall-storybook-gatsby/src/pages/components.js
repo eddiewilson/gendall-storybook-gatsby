08e3ca7c57f417009b82e4054da992dbff0ac321
Ed Wilson <ed@gendall.co.uk>
2021-01-04 13:09:07 +0000

    wording

--- gendall-storybook-gatsby/src/pages/components.js
+++ gendall-storybook-gatsby/src/pages/components.js
@@ -8,6 +8,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Components</h1>
     <p>Welcome to the components page.</p>
+    <p>This repo is for building components for the Gendall storybook library. <br/>Please <code>npm run storybook</code> to checkout and develop components. </p>
   </Layout>
 )
 
